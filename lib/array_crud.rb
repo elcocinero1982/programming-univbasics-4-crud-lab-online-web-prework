30a867ab36ae6d6f8840cbaeef67718f9d54c3c7
jose luis <cortezluisjose@gmail.com>
2019-06-30 03:54:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-my_array = array.new["coconut"]
-add_element_to_end_of_array.push["orange"]
+
+
 end 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array.unshift("strawberry")
