263d08e08e4db585a0f5f439ea80f67222877b7d
jose luis <cortezluisjose@gmail.com>
2019-06-30 03:29:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   
- add_element_to_end_of_array.push 
+ add_element_to_end_of_array.push << orange
 end 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array.unshift("strawberry")
